584deb904171a143d96120fd0d09fb445ede12d2
Hema20042025 <163738640+Hema20042025@users.noreply.github.com>
2024-10-06 12:37:51 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import pandas as pd
 import requests
 import pickle
-import gzip
 
 # TMDb API key
 tmdb_api_key = '196506d3ecb6b9c36355886bcd6d92ab'
+
 
 # Function to fetch details from TMDb
 def fetch_anime_details(title, api_key):
@@ -17,21 +17,24 @@ def fetch_anime_details(title, api_key):
     response = requests.get(url, params=params)
     data = response.json()
 
+    # Check if results exist
     if data['results']:
-        first_result = data['results'][0]
+        first_result = data['results'][0]  # Taking the first result as the best match
         return {
             'tmdb_id': first_result['id'],
-            'title': first_result.get('name', title),
+            'title': first_result.get('name', title),  # Get the name or fallback to original title
             'poster_path': first_result.get('poster_path', None)
         }
     else:
         return None
+
 
 # Function to get full URL of the poster image
 def get_poster_url(poster_path):
     if poster_path:
         return f"https://image.tmdb.org/t/p/w500{poster_path}"
     return None
+
 
 # Function to recommend animes
 def recommend(anime):
@@ -55,19 +58,11 @@ def recommend(anime):
             })
     return recommended_animes
 
-# Load data with compression
+
+# Load data
 animes_list = pickle.load(open('animes.pkl', 'rb'))
 animes = pd.DataFrame(animes_list)
-
-
-# Load the compressed similarity.pkl.gz file
-try:
-    with gzip.open('updated_similarity.pkl.gz', 'rb') as f:
-        similarity = pickle.load(f)
-    print("Loaded similarity data successfully!")
-except Exception as e:
-    print(f"Error loading similarity data: {e}")
-
+similarity = pickle.load(open('similarity.pkl', 'rb'))
 
 # Streamlit app
 st.title("Anime Recommendation System")
@@ -77,7 +72,10 @@ selected_anime_name = st.selectbox('***Select an Anime***', animes['name'].value
 if st.button('Recommend'):
     recommendations = recommend(selected_anime_name)
 
+    # Create columns for horizontal layout
     cols = st.columns(len(recommendations))
+
+    # Display each recommendation in a separate column
     for col, recommendation in zip(cols, recommendations):
         with col:
             st.write(recommendation['title'])
@@ -85,5 +83,7 @@ if st.button('Recommend'):
                 st.image(recommendation['poster_url'], use_column_width=True)
             else:
                 st.write("***Poster Not Available***")
+
+
 
 
